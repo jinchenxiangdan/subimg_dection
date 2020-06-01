406d8f13e4f8e9c132ec85dccfd69a6383002b8f
shawn <jinchenxiangdan@gmail.com>
2020-05-31 18:57:52 -0700

    added test file 2

--- ocr_text_location.py
+++ ocr_text_location.py
@@ -1,4 +1,5 @@
 import cv2
+import imutils as imutils
 import pytesseract 
 import glob
 import numpy as np
@@ -13,8 +14,8 @@ import numpy as np
 pytesseract.pytesseract.tesseract_cmd = r'I:\\Tesseract\\tesseract.exe'
 
 # 
-img = cv2.imread('resources\\test_img_1.png')
-cv2.imshow("img",img)
+# img = cv2.imread('resources\\test_img_1.png')
+# cv2.imshow("img",img)
 
 ###
 # Pytesseract
@@ -33,34 +34,54 @@ cv2.imshow("img",img)
 ###
 
 # handle the template img
-template_img = cv2.imread('resources\\building1_roof.png')
+template_img = cv2.imread('resources\\building2.png')
 template_gray_img = cv2.cvtColor(template_img, cv2.COLOR_BGR2GRAY)
 detect = cv2.Canny(template_gray_img, 50, 200)
-(tH, tw) = detect.shape[:2]
+(tH, tW) = detect.shape[:2]
 cv2.imshow("eg", detect)
 
-#
-# loop over the images to find the template in
-for imagePath in glob.glob(args["images"] + "/*.jpg"):
-	# load the image, convert it to grayscale, and initialize the
-	# bookkeeping variable to keep track of the matched region
-	image = cv2.imread(imagePath)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	found = None
-	# loop over the scales of the image
-	for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-		# resize the image according to the scale, and keep track
-		# of the ratio of the resizing
-		resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
-		r = gray.shape[1] / float(resized.shape[1])
-		# if the resized image is smaller than the template, then break
-		# from the loop
-		if resized.shape[0] < tH or resized.shape[1] < tW:
-			break
+# load the whole img
+test_1_img = cv2.imread('resources\\test_2.png')
+gray = cv2.cvtColor(test_1_img, cv2.COLOR_BGR2GRAY)
+found = None
+for scale in np.linspace(0.1, 5.0, 50)[::-1]:
+	# resize the image according to the scale, and keep track
+	# of the ratio of the resizing
+	resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+	r = gray.shape[1] / float(resized.shape[1])
+	# if the resized image is smaller than the template, then break
+	# from the loop
+	if resized.shape[0] < tH or resized.shape[1] < tW:
+		break
 
+	# detect edges in the resized, grayscale image and apply template
+	# matching to find the template in the image
+	edged = cv2.Canny(resized, 50, 200)
+	result = cv2.matchTemplate(edged, detect, cv2.TM_CCOEFF)
+	(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+	# check to see if the iteration should be visualized
 
-
-
+	# draw a bounding box around the detected region
+	clone = np.dstack([edged, edged, edged])
+	cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+		(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+	cv2.imshow("Visualize", clone)
+	cv2.waitKey(0)
+	# if we have found a new maximum correlation value, then update
+	# the bookkeeping variable
+	if found is None or maxVal > found[0]:
+		found = (maxVal, maxLoc, r)
+# unpack the bookkeeping variable and compute the (x, y) coordinates
+# of the bounding box based on the resized ratio
+(_, maxLoc, r) = found
+(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+# draw a bounding box around the detected result and display the image
+cv2.rectangle(test_1_img, (startX, startY), (endX, endY), (0, 0, 255), 2)
+cv2.imshow("Image", test_1_img)
+cv2.resizeWindow("Image", 1080, 560)
+# cv2.imshow("Resized Img", cv2.resize(test_1_img, (1080, 540)) )
+cv2.waitKey(0)
 
 
 
